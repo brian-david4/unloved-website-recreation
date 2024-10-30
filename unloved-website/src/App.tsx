7a778d235f858f45fea1ba84c735ad3b43925ebd
Brian David <bdavid1530@gmail.com>
2024-10-30 17:06:29 +0000

    update styling for each section
    create p tag class : `sectionDetails`

--- unloved-website/src/App.tsx
+++ unloved-website/src/App.tsx
@@ -6,24 +6,46 @@ function App() {
       <main>
         <ScrollSection></ScrollSection>
         <ScrollSection>
-          <h1>we are the unloved.</h1>
+          <p className="sectionDetails">we are the unloved.</p>
         </ScrollSection>
-        <ScrollSection>we are an independent creative studio</ScrollSection>
+
         <ScrollSection>
-          we use design and digital to find answers to your questions
+          <p className="sectionDetails">
+            we are an independent creative studio
+          </p>
         </ScrollSection>
-        <ScrollSection>creative problem solving is a process,</ScrollSection>
-        <ScrollSection>a process we live by.</ScrollSection>
-        <ScrollSection>are you feeling unloved?</ScrollSection>
+
         <ScrollSection>
-          beloved@theunloved.co.uk <br />
-          <br />
-          +44 (0)116 218 2233 <br />
-          <br />
-          instagram twitter
+          <p className="sectionDetails">
+            we use design and digital to find answers to your questions
+          </p>
+        </ScrollSection>
+
+        <ScrollSection>
+          <p className="sectionDetails">
+            creative problem solving is a process,
+          </p>
         </ScrollSection>
         <ScrollSection>
-          <p>#beautifulbydesign</p>
+          <p className="sectionDetails">a process we live by.</p>
+        </ScrollSection>
+        <ScrollSection>
+          <p className="sectionDetails">are you feeling unloved?</p>
+        </ScrollSection>
+        <ScrollSection>
+          <p className="sectionDetails">
+            <a href="mailto:beloved@theunloved.co.uk">
+              beloved@theunloved.co.uk
+            </a>{" "}
+            <a href="tel:01162182233">+44 (0)116 218 2233</a>
+            <div className="socials">
+              <a href="https://www.instagram.com/the_un.loved/">instagram</a>{" "}
+              <a href="https://x.com/the_un_loved">twitter</a>
+            </div>
+          </p>
+        </ScrollSection>
+        <ScrollSection>
+          <p className="sectionDetails">#beautifulbydesign</p>
         </ScrollSection>
 
         <div className="videopage">
